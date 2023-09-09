8bc699bee74b352ea177c166780fc4af3d878fab
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-09-09 13:25:39 +0700

    count total activity data by month,week,and day

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,26 +1,91 @@
+const moment = require("moment");
 const Activity = require("../models/Activity");
-const Schedule = require("../models/Schedule");
 const Task = require("../models/Task");
 const User = require("../models/User");
 
+async function countDocumentsByTime(model, timestamp, timePeriod) {
+  try {
+    let filter = {};
+
+    // Define the date range based on the time period
+    switch (timePeriod) {
+      case "today":
+        filter = {
+          [timestamp]: {
+            $gte: moment().startOf("day").toDate(),
+            $lt: moment().endOf("day").toDate(),
+          },
+        };
+        break;
+      case "week":
+        filter = {
+          [timestamp]: {
+            $gte: moment().startOf("week").toDate(),
+            $lt: moment().endOf("week").toDate(),
+          },
+        };
+        break;
+      case "month":
+        filter = {
+          [timestamp]: {
+            $gte: moment().startOf("month").toDate(),
+            $lt: moment().endOf("month").toDate(),
+          },
+        };
+        break;
+      case "all":
+        // No date filter for "all" time
+        break;
+      default:
+        throw new Error("Invalid time period");
+    }
+
+    const count = await model.countDocuments(filter);
+    return count;
+  } catch (error) {
+    throw error;
+  }
+}
+
 exports.dashboardAdmin = async (req, res) => {
   try {
-    const totalUsers = await User.countDocuments();
-    const totalActivity = await Activity.countDocuments();
-    const totalTheaterSchedule = await Schedule.find({
-      isOnWeekSchedule: true,
-    }).countDocuments();
+    const totalUsers = await countDocumentsByTime(User, "createdAt", "all");
+    
+    const totalActivityToday = await countDocumentsByTime(
+      Activity,
+      "timestamp",
+      "today"
+    );
+    const totalActivityWeek = await countDocumentsByTime(
+      Activity,
+      "timestamp",
+      "week"
+    );
+    const totalActivityMonth = await countDocumentsByTime(
+      Activity,
+      "timestamp",
+      "month"
+    );
+    const totalActivityAll = await countDocumentsByTime(
+      Activity,
+      "timestamp",
+      "all"
+    );
     const totalTask = await Task.find({
       active: true,
-    }).countDocuments()
+    }).countDocuments();
 
     res.status(200).json({
       totalUsers,
-      totalActivity,
-      totalTheaterSchedule,
+      totalActivity: {
+        today: totalActivityToday,
+        week: totalActivityWeek,
+        month: totalActivityMonth,
+        all: totalActivityAll,
+      },
       totalTask,
     });
   } catch (error) {
-    res.status(500).json({ error: "Failed to delete admin user." });
+    res.status(500).json({ error: "Failed to fetch data." });
   }
 };
