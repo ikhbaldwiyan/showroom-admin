4bed202091bdb4dc8ab2ae218cd9d00a2b4ff732
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-09-21 15:48:11 +0700

    show activity log on live detail

--- controller/historyLiveController.js
+++ controller/historyLiveController.js
@@ -1,3 +1,4 @@
+const Activity = require("../models/Activity");
 const HistoryLive = require("../models/HistoryLive");
 
 exports.getAllLiveHistory = async (req, res) => {
@@ -14,17 +15,17 @@ exports.getLiveDetail = async (req, res) => {
   const { id } = req.params;
   try {
     const liveData = await HistoryLive.findOne({ live_id: parseInt(id) });
+    const activityLog = await Activity.find({ live_id: parseInt(id) }).populate("user");
 
     if (!liveData) {
       return res.status(404).json({ message: "Live not found" });
     }
 
-    res.json(liveData);
+    res.json({ liveData, activityLog });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
 
 // Controller to get details of live events by array of _ids
 exports.getLiveDetailsByIds = async (req, res) => {
@@ -34,12 +35,12 @@ exports.getLiveDetailsByIds = async (req, res) => {
     const liveData = await HistoryLive.find({ live_id: { $in: ids } });
 
     if (!liveData || liveData.length === 0) {
-      return res.status(404).json({ message: 'Live not found' });
+      return res.status(404).json({ message: "Live not found" });
     }
 
     res.json(liveData);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Internal server error' });
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
