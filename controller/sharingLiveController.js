895ce89ed338740bcef5f68c4afbb68090aa2277
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-10-16 15:20:52 +0700

    handle user already registered sharing live

--- controller/sharingLiveController.js
+++ controller/sharingLiveController.js
@@ -2,18 +2,19 @@ const SharingLive = require("../models/SharingLive");
 
 exports.getAllSharingLive = async (req, res) => {
   try {
-    const sharingLive = await SharingLive.find().populate({
-      path: 'schedule_id',
-      select: 'showDate showTime setlist',
-      populate: {
-        path: "setlist",
-        select: 'name originalName',
-      }
-    })
-    .populate({
-      path: 'user_id',
-      select: 'name user_id',
-    });
+    const sharingLive = await SharingLive.find()
+      .populate({
+        path: "schedule_id",
+        select: "showDate showTime setlist",
+        populate: {
+          path: "setlist",
+          select: "name originalName",
+        },
+      })
+      .populate({
+        path: "user_id",
+        select: "name user_id",
+      });
     res.json(sharingLive);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
@@ -29,10 +30,21 @@ exports.createSharingLive = async (req, res) => {
       discord_name,
       status,
     });
+    const registeredUser = await SharingLive.findOne({
+      user_id,
+      schedule_id,
+    });
+
+    if (registeredUser) {
+      return res
+        .status(400)
+        .json({ message: "User is already registered for this sharing live." });
+    }
+
     await newsharingLive.save();
     res.status(201).json(newsharingLive);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -42,26 +54,26 @@ exports.getSharingLiveDetail = async (req, res) => {
     const sharingLiveId = req.params.id;
     const sharingLive = await SharingLive.findById(sharingLiveId)
       .populate({
-        path: 'schedule_id',
-        select: 'showDate showTime setlist',
+        path: "schedule_id",
+        select: "showDate showTime setlist",
         populate: {
           path: "setlist",
-          select: 'name originalName',
-        }
+          select: "name originalName",
+        },
       })
       .populate({
-        path: 'user_id',
-        select: 'name user_id',
+        path: "user_id",
+        select: "name user_id",
       });
 
     if (!sharingLive) {
-      return res.status(404).json({ error: 'Sharing live event not found' });
+      return res.status(404).json({ error: "Sharing live event not found" });
     }
 
     res.json(sharingLive);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -76,26 +88,26 @@ exports.updateSharingLive = async (req, res) => {
       { new: true }
     )
       .populate({
-        path: 'schedule_id',
-        select: 'showDate showTime setlist',
+        path: "schedule_id",
+        select: "showDate showTime setlist",
         populate: {
-          path: 'setlist',
-          select: 'name originalName',
+          path: "setlist",
+          select: "name originalName",
         },
       })
       .populate({
-        path: 'user_id',
-        select: 'name user_id',
+        path: "user_id",
+        select: "name user_id",
       });
 
     if (!sharingLive) {
-      return res.status(404).json({ error: 'Sharing live event not found' });
+      return res.status(404).json({ error: "Sharing live event not found" });
     }
 
     res.json(sharingLive);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -108,12 +120,12 @@ exports.deleteSharingLive = async (req, res) => {
     );
 
     if (!deletedSharingLive) {
-      return res.status(404).json({ error: 'Sharing live event not found' });
+      return res.status(404).json({ error: "Sharing live event not found" });
     }
 
-    res.json({ message: 'Sharing live users deleted successfully' });
+    res.json({ message: "Sharing live users deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
