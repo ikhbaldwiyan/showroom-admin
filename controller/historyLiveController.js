0bed8f69b0689d15153bf6a3dbaeecc93487ef5a
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-12-23 17:48:56 +0700

    add pagination and profile data to history live list

--- controller/historyLiveController.js
+++ controller/historyLiveController.js
@@ -4,8 +4,54 @@ const fetchService = require("../utils/fetchService");
 
 exports.getAllLiveHistory = async (req, res) => {
   try {
-    const liveHistory = await HistoryLive.find({});
-    res.json(liveHistory);
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const perPage = req.query.perPage ?? 10;
+
+    const totalCount = await HistoryLive.countDocuments({});
+    const totalPages = Math.ceil(totalCount / perPage);
+
+    let allLiveHistory = await HistoryLive.find({}); // Fetch all data without pagination
+    allLiveHistory = allLiveHistory.reverse(); // Reverse the entire dataset
+
+    const startIdx = (page - 1) * perPage;
+    const endIdx = startIdx + perPage;
+    const liveHistory = allLiveHistory.slice(startIdx, endIdx); // Apply pagination to the reversed dataset
+
+    const profilePromises = liveHistory.map(async (history) => {
+      const profileUrl = `https://www.showroom-live.com/api/room/profile?room_id=${
+        history?.roomId ?? 332503
+      }`;
+      const profileApi = await fetchService(profileUrl, res);
+
+      const watchingUser = await Activity.find({
+        live_id: history?.live_id,
+        log_name: "Watch",
+      }).countDocuments({});
+
+      return {
+        watchingUser,
+        liveHistory: history,
+        profile: {
+          room_name: profileApi.data.room_name,
+          image: profileApi.data.image,
+          views: profileApi.data.view_num,
+          is_onlive: profileApi.data.is_onlive,
+          current_live_started_at: profileApi.data.current_live_started_at,
+        },
+      };
+    });
+
+    const combinedData = await Promise.all(profilePromises);
+
+    res.json({
+      data: combinedData,
+      pagination: {
+        currentPage: page,
+        totalPages: totalPages,
+        totalData: totalCount,
+        perPage
+      },
+    });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
@@ -16,7 +62,7 @@ exports.getLiveDetail = async (req, res) => {
   const { id } = req.params;
   try {
     const liveData = await HistoryLive.findOne({ live_id: parseInt(id) });
-    
+
     const populateData = {
       path: "user",
       select: "name user_id avatar",
