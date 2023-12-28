54d0aabc2038f6bf286d84c02cea4a8eedb43889
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-12-28 19:09:37 +0700

    implement live notif discord for IDN Live

--- routes/discordRoute.js
+++ routes/discordRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const userPermissionsController = require("../controller/userControlloer");
 const discordController = require('../controller/discordController')
-const middleware = require("../utils/jwtMiddleware");
+const idnLivesController = require('../controller/idnLivesController');
 
 const router = express.Router();
 
 router.get("/role", discordController.getUserRoles);
+router.get('/idn-lives', idnLivesController.sendDiscordNotif);
 
 module.exports = router;
